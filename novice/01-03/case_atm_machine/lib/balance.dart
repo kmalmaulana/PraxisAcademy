6920e2508d4c0ce103e60cb38c43468df565ba1a
Akmal Maulana <akmalmaulana701@gmail.com>
2019-08-19 01:54:36 +0700

    atm machine

---
+++ novice/01-03/case_atm_machine/lib/balance.dart
@@ -0,0 +1,12 @@
+import 'mainn.dart';
+
+class BalanceInquiry extends ATMMachine {
+  static double blc = 0;
+  static void set_balance(double b) {
+    blc = b;
+  }
+
+  static double get_balance() {
+    return blc;
+  }
+}
