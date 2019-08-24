9d7ee67f8549165e940032f2c205615cbe465085
Akmal Maulana <akmalmaulana701@gmail.com>
2019-08-25 03:57:08 +0700

    play with Row & Column

--- novice/02-02/widgets_layout_implement/lib/main.dart
+++ novice/02-02/widgets_layout_implement/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   //runApp function
   runApp(new MaterialApp(
+    title: "Artic",
     home: new homePage(),
   ));
 }
@@ -13,20 +14,68 @@ class homePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: new Center(
-        child: new Container(
-          //box
-          width: 200.0,
-          height: 100.0,
-          color: Colors.yellow,
-          child: new Center(
-            child: new Text(
-              'this is Home Page',
-              style: new TextStyle(color: Colors.black),
+      backgroundColor: Colors.white,
+      appBar: new AppBar(
+        backgroundColor: Colors.yellowAccent,
+        leading: new Icon(
+            IconData(
+              59530,
+              fontFamily: 'MaterialIcons',
             ),
+            color: Colors.black),
+        title: new Center(
+          child: new Text(
+            "Artic",
+            style: TextStyle(color: Colors.black),
           ),
+        ),
+        actions: <Widget>[
+          new Icon(
+              IconData(
+                59574,
+                fontFamily: 'MaterialIcons',
+              ),
+              color: Colors.black)
+        ],
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Row(
+              children: <Widget>[
+                new Icon(IconData(57671, fontFamily: 'MaterialIcons'),
+                    size: 40.0, color: Colors.black),
+                new Icon(IconData(58430, fontFamily: 'MaterialIcons'),
+                    size: 40.0, color: Colors.black),
+                new Icon(
+                  IconData(57673, fontFamily: 'MaterialIcons'),
+                  size: 40.0,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            new Icon(IconData(58430, fontFamily: 'MaterialIcons'),
+                size: 40.0, color: Colors.black),
+            new Row(
+              children: <Widget>[
+                new Icon(
+                  IconData(57673, fontFamily: 'MaterialIcons'),
+                  size: 40.0,
+                  color: Colors.black,
+                ),
+                new Icon(
+                  IconData(58430, fontFamily: 'MaterialIcons'),
+                  size: 40.0,
+                  color: Colors.black,
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 }
+
+////59485
+//57673 58430 57671
